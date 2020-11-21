82a123dacf4a2cfa6cae99198a5035cdd03b1b67
Kevin Lot <klot@keyclic.com>
2020-11-21 01:45:38 +0100

    Adapt new imports after globalize hasSupport method.

--- packages/native_pdf_renderer/example/lib/main.dart
+++ packages/native_pdf_renderer/example/lib/main.dart
@@ -3,12 +3,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:native_pdf_renderer/native_pdf_renderer.dart';
-// ignore: uri_does_not_exist
-import 'has_support_stub.dart'
-    // ignore: uri_does_not_exist
-    if (dart.library.html) 'has_support_browser.dart'
-    // ignore: uri_does_not_exist
-    if (dart.library.io) 'has_support_io.dart';
 
 void main() => runApp(ExampleApp());
 
