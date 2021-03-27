3ae2f1666f6513a1058df69d8b5fc190f955003c
Jon Salmon <jonathan.salmon@hotmail.co.uk>
2021-03-27 13:16:37 +0000

    Added Windows example

---
+++ packages/native_pdf_renderer/example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <native_pdf_renderer/native_pdf_renderer_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  NativePdfRendererPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("NativePdfRendererPlugin"));
+}
