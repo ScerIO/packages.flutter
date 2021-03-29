6733f891d246ebf2c6318083e42c4ccc562a9a49
Jon Salmon <jonathan.salmon@hotmail.co.uk>
2021-03-29 01:38:54 +0100

    CLang formatting for CI

--- packages/native_pdf_renderer/windows/native_pdf_renderer.h
+++ packages/native_pdf_renderer/windows/native_pdf_renderer.h
@@ -1,85 +1,88 @@
 #ifndef NATIVE_PDF_RENDERER_H_
 #define NATIVE_PDF_RENDERER_H_
 
-#include <vector>
-#include <string>
 #include <memory>
+#include <string>
+#include <vector>
 
 #include <fpdfview.h>
 
-namespace native_pdf_renderer
-{
-    enum ImageFormat
-    {
-        JPEG = 0,
-        PNG = 1,
-    };
+namespace native_pdf_renderer {
+enum ImageFormat {
+  JPEG = 0,
+  PNG = 1,
+};
 
-    struct CropDetails
-    {
-        int crop_x;
-        int crop_y;
-        int crop_height;
-        int crop_width;
-    };
+struct CropDetails {
+  int crop_x;
+  int crop_y;
+  int crop_height;
+  int crop_width;
+};
 
-    struct PageDetails
-    {
-        const int width;
-        const int height;
+struct PageDetails {
+  const int width;
+  const int height;
 
-        PageDetails(int width, int height) : width(width), height(height) {}
-    };
+  PageDetails(int width, int height) : width(width), height(height) {}
+};
 
-    struct PageRender
-    {
-        const std::vector<uint8_t> data;
-        const int width;
-        const int height;
+struct PageRender {
+  const std::vector<uint8_t> data;
+  const int width;
+  const int height;
 
-        PageRender(std::vector<uint8_t> data, int width, int height) : data(data), width(width), height(height) {}
-    };
+  PageRender(std::vector<uint8_t> data, int width, int height)
+      : data(data), width(width), height(height) {}
+};
 
-    class Document
-    {
-    private:
-        std::vector<uint8_t> data;
+class Document {
+ private:
+  std::vector<uint8_t> data;
 
-    public:
-        Document(std::vector<uint8_t> data, std::string id);
-        Document(std::string file, std::string id);
+ public:
+  Document(std::vector<uint8_t> data, std::string id);
+  Document(std::string file, std::string id);
 
-        ~Document();
+  ~Document();
 
-        std::string id;
-        FPDF_DOCUMENT document;
+  std::string id;
+  FPDF_DOCUMENT document;
 
-        int getPageCount(void);
-    };
+  int getPageCount(void);
+};
 
-    class Page
-    {
-    private:
-        FPDF_PAGE page;
+class Page {
+ private:
+  FPDF_PAGE page;
 
-    public:
-        Page(std::shared_ptr<Document> doc, int index, std::string id);
-        ~Page();
+ public:
+  Page(std::shared_ptr<Document> doc, int index, std::string id);
+  ~Page();
 
-        std::string id;
+  std::string id;
 
-        PageDetails getDetails();
-        PageRender render(int width, int height, ImageFormat format, unsigned long background, CropDetails *crop);
-    };
+  PageDetails getDetails();
+  PageRender render(int width,
+                    int height,
+                    ImageFormat format,
+                    unsigned long background,
+                    CropDetails* crop);
+};
 
-    std::shared_ptr<Document> openDocument(std::vector<uint8_t> data);
-    std::shared_ptr<Document> openDocument(std::string name);
+std::shared_ptr<Document> openDocument(std::vector<uint8_t> data);
+std::shared_ptr<Document> openDocument(std::string name);
 
-    void closeDocument(std::string id);
-    std::shared_ptr<Page> openPage(std::string docId, int index);
-    void closePage(std::string id);
-    PageRender renderPage(std::string id, int width, int height, ImageFormat format, std::string backgroundStr, CropDetails *crop);
+void closeDocument(std::string id);
+std::shared_ptr<Page> openPage(std::string docId, int index);
+void closePage(std::string id);
+PageRender renderPage(std::string id,
+                      int width,
+                      int height,
+                      ImageFormat format,
+                      std::string backgroundStr,
+                      CropDetails* crop);
 
-}
+}  // namespace native_pdf_renderer
 
 #endif
