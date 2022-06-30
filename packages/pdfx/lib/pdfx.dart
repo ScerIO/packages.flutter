05787451bc435bba8c40246d59b46cdc76ce8933
Eka Setiawan Saputra <mr.poetra22@gmail.com>
2022-06-30 15:05:03 +0700

    fix: remove exports pdfx_plugin for web

--- packages/pdfx/lib/pdfx.dart
+++ packages/pdfx/lib/pdfx.dart
@@ -1,7 +1,6 @@
 export 'src/renderer/has_pdf_support.dart';
 export 'src/renderer/interfaces/document.dart';
 export 'src/renderer/interfaces/page.dart';
-export '/src/renderer/web/pdfx_plugin.dart';
 export 'src/renderer/rgba_data.dart';
 
 export 'package:photo_view/photo_view.dart';
