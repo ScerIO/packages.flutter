0a1f053e1bf6e4310d051b0855f555f5a21fb121
SumeetMakhija <sumeet.makhija@sevenlakes.com>
2023-04-19 12:41:12 +0530

    [pdfx] linter fix

--- packages/pdfx/lib/src/viewer/pinch/pdf_controller_pinch.dart
+++ packages/pdfx/lib/src/viewer/pinch/pdf_controller_pinch.dart
@@ -255,11 +255,6 @@ class PdfControllerPinch extends TransformationController
   void _detach() {
     _state = null;
   }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
 }
 
 enum _PdfPageLoadingStatus {
