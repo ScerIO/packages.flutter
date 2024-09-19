9392c01d5e6e10a1d7b898a100b14ba3f5c7f8f4
Emilio Dalla Torre <emilio.dalla.torre@pwc.com>
2024-09-19 14:59:56 +0200

    Updated install script to check version consistency

--- packages/pdfx/bin/install_web.dart
+++ packages/pdfx/bin/install_web.dart
@@ -1,38 +1,78 @@
-import 'dart:io';
+import "dart:io";
 
-const findString = '<body>';
+const String pdfjsVersion = "4.6.82";
 
-const _template = """$findString
+const String findString = "<body>";
+const String _template = """$findString
   <script src="https://cdn.jsdelivr.net/npm/pdfjs-dist@4.6.82/build/pdf.min.mjs" type="module"></script>
   <script type="module">
   var { pdfjsLib } = globalThis;
   pdfjsLib.GlobalWorkerOptions.workerSrc = "https://cdn.jsdelivr.net/npm/pdfjs-dist@4.6.82/build/pdf.worker.mjs";
 
   var pdfRenderOptions = {
-    cMapUrl: 'https://cdn.jsdelivr.net/npm/pdfjs-dist@4.6.82/cmaps/',
+    cMapUrl: "https://cdn.jsdelivr.net/npm/pdfjs-dist@4.6.82/cmaps/",
     cMapPacked: true,
   }
   </script>""";
 
 void main(List<String> args) async {
-  stdout.writeln('[Modify web/index.hml by addition pdfjs]');
+  stdout.writeln("[Modify web/index.hml by addition pdfjs]");
 
-  final htmlFile = File('web/index.html');
+  final htmlFile = File("web/index.html");
 
   if (!await htmlFile.exists()) {
-    stdout.writeln('Cannot find web/index.html');
+    stdout.writeln("Cannot find web/index.html");
     exit(1);
   }
 
-  final document = await htmlFile.readAsString();
+  final String document = await htmlFile.readAsString();
 
-  if (document.contains('pdfRenderOptions')) {
-    stdout.writeln('Already installed, operation aborted');
-    exit(2);
+  if (document.contains("pdfRenderOptions")) {
+    stdout.writeln("Already installed, checking version consistency");
+
+    final VersionConsistencyChecker checker = VersionConsistencyChecker(expectedVersion: "4.6.82");
+    if (checker.checkVersion(document)) {
+      stdout.writeln("Version is consistent");
+
+      exit(2);
+    } else {
+      stdout.writeln("Version is inconsistent, fixing version");
+      final resultDocument = checker.fixVersion(document);
+      await htmlFile.writeAsString(resultDocument);
+      stdout.writeln("Installation successful");
+
+      exit(0);
+    }
   }
 
   final resultDocument = document.replaceFirst(findString, _template);
   await htmlFile.writeAsString(resultDocument);
 
-  stdout.writeln('installation successfully');
+  stdout.writeln("installation successfully");
+}
+
+class VersionConsistencyChecker {
+  static final RegExp versionRegex = RegExp(r"pdfjs-dist@(\d+\.\d+\.\d+)");
+
+  final String expectedVersion;
+
+  VersionConsistencyChecker({required this.expectedVersion});
+
+  bool checkVersion(final String indexContent) {
+    if (versionRegex.hasMatch(indexContent)) {
+      final Iterable<RegExpMatch> matches = versionRegex.allMatches(indexContent);
+
+      for (final match in matches) {
+        if (match.group(1) != expectedVersion) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  String fixVersion(final String indexContent) {
+    return indexContent.replaceAll(versionRegex, "pdfjs-dist@$expectedVersion");
+  }
 }
